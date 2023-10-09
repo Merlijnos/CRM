d9faa7642f2590decc5d8cb473135eab2d15494c
Merlijnos <99071140@mydavinci.nl>
2023-10-09 10:30:08 +0200

    swapped

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -1,14 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
 # Create your views here.
 from .models import *
+from .forms import OrderForm
 
 def home(request):
     orders = Order.objects.all()
     customers = Customer.objects.all()
 
-    context = {"orders": orders, "customers": customers}
+    total_customers = customers.count()
+
+    total_orders = orders.count()
+    delivered = orders.filter(status="Delivered").count()
+    pending = orders.filter(status="Pending").count()
+
+    context = {"orders": orders, "customers": customers, "total_orders": total_orders, "delivered": delivered, "pending": pending}
 
     return render(request, "accounts/dashboard.html", context)
 
@@ -19,6 +25,23 @@ def products(request):
     return render(request, "accounts/products.html", {"products": products})
 
 
-def customer(request):
+def customer(request, pk_test):
+    customer = Customer.objects.get(id=pk_test)
 
-    return render(request, "accounts/customer.html")
+    orders = customer.order_set.all()
+    order_count = orders.count()
+    context = {"customer": customer, "orders": orders, "order_count": order_count}
+    return render(request, "accounts/customer.html", context)
+
+def createOrder(request):
+
+    form = OrderForm()
+    if request.method == "POST":
+        # print("Printing POST:", request.POST)
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("/")
+
+    context = {'form': form}
+    return render(request, "accounts/order_form.html", context)
